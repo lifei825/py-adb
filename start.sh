9393683255ce5db5b26fa7dc0dbc3a5e81791b52
lifei825 <747553934@qq.com>
2021-07-05 20:41:10 +0800

    u

--- start.sh
+++ start.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 mix2s () {
-python3 main.py -d mix2s -t round_all
+#/usr/bin/python3 main.py -d mix2s -t round_all
+/usr/bin/nohup /usr/bin/python3 -u  /root/py-adb/main.py -d mix2s -t round_all > mix2s.out 2>&1 &
 }
 
 huawei() {
-python3 main.py -d huawei -t round_all
+/usr/bin/python3 main.py -d huawei -t round_all
 }
 
 redmi() {
-python3 main.py -d redmi -t round_all
+/usr/bin/nohup /usr/bin/python3 -u  /root/py-adb/main.py -d redmi -t round_all > redmi.out 2>&1 &
 }
 
-if [ $1 == 'mix2s' ];then
- mix2s
-elif [ $1 == 'redmi' ];then
- redmi
-elif [ $1 == 'huawei' ];then
- huawei
+
+#if ! /usr/bin/ps -ef | grep $1 | grep -v grep &>/dev/null;then 
+if ! /usr/bin/ps -ef | /usr/bin/grep "$1 -t round_all" | /usr/bin/grep -v grep;then 
+	if [ $1 == 'mix2s' ];then
+	 mix2s
+	elif [ $1 == 'redmi' ];then
+	 redmi
+	elif [ $1 == 'huawei' ];then
+	 huawei
+	fi
+else
+	/usr/bin/echo $1 already running
 fi
